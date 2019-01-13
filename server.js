20a6f696149cd98a355230304f3f661237c8f587
Konstantyn Bykhkalo <byhkalo.konstantyn@gmail.com>
2019-01-13 14:52:49 +0100

    update port for Heroku deploy

--- server.js
+++ server.js
@@ -11,4 +11,8 @@ app.get('/*', function(req, res) {
 });
 
 // default Heroku port
-app.listen(process.env.PORT || 5000);
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log("App is running on port " + port);
+});
